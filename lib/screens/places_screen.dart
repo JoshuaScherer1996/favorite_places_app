14b77146cd91135dfd9f482a852676a97cceaacf
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-06 21:13:15 +0100

    Finished up Challenge number five by sharing the State throughout the provider between screens.

--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -1,19 +1,16 @@
+import 'package:favorite_places_app/providers/user_places.dart';
 import 'package:favorite_places_app/screens/add_place_screen.dart';
 import 'package:favorite_places_app/widgets/place_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatefulWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  State<PlacesScreen> createState() {
-    return _PlacesScreenState();
-  }
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
 
-class _PlacesScreenState extends State<PlacesScreen> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -30,8 +27,8 @@ class _PlacesScreenState extends State<PlacesScreen> {
           )
         ],
       ),
-      body: const PlaceList(
-        places: [],
+      body: PlaceList(
+        places: userPlaces,
       ),
     );
   }
