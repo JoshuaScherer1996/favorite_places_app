61f4c68e65849ab5c007ac5a2d0748133040fb14
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-11 20:58:41 +0100

    enabled the different path packages to safe the image to the right paths.

--- lib/providers/user_places_provider.dart
+++ lib/providers/user_places_provider.dart
@@ -1,12 +1,18 @@
 import 'package:favorite_places_app/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(name: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDirectory = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDirectory.path}/$filename');
+
+    final newPlace = Place(name: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
